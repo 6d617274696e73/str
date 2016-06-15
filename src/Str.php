dc8e0132a7973e48ae7a2ec60deb7818c5543a70
Martins Dumbris <martins.dumbris@gmail.com>
2016-06-15 09:20:19 +0300

    Package init

---
+++ src/Str.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tattvika\Str;
+
+class Str
+{
+
+    /**
+     * Initialize.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+}
---
+++ tests/StrTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tattvika\Str\Tests;
+
+use Tattvika\Str\Str;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class StrTest extends PHPUnit
+{
+    public function testExample()
+    {
+        $str = new Str();
+
+        $this->assertTrue(true);
+    }
+}
